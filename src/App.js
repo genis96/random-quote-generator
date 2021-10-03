502060217e9564a3cdb481a6d95c71c276422fe4
Genis Bardales <genis96@yahoo.com>
2021-10-02 23:54:35 -0400

    the random generator works

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Generate from './Generate';
+import './App.css';
 
+import fbIcon from './images/facebook.svg';
+import twitterIcon from './images/twitter.svg';
 
 function App() {
 
@@ -9,18 +12,37 @@ function App() {
   const [author, setAuthor] = useState('');
 
   useEffect(() => {
-    axios.get(
+    fetch(
       'https://stoicquotesapi.com/v1/api/quotes/random'
     )
-    .then(res => {
-      setQuote(res.data);
-      console.log(res.data)
-    }).catch(error => console.log(error));
+    .then(res => res.json()) 
+    .then((quote) => {
+      setQuote(quote.body);
+      setAuthor(quote.author);
+      console.log(quote)
+    })
+    
   }, []);
 
+
+  let activateButton = () => {
+    fetch(
+      'https://stoicquotesapi.com/v1/api/quotes/random'
+    )
+    .then(res => res.json()) 
+    .then((quote) => {
+      setQuote(quote.body);
+      setAuthor(quote.author);
+    })
+  }
+
   return (
-    <div className="App">
-      <h1>Hi</h1>
+    <div className='App'>
+      <div className='quote'>
+        <h1>{quote}</h1>
+        <small> {author} </small>
+      </div> <br></br>
+      <button className='new-quote' onClick={activateButton}>New Quote</button>
     </div>
   );
 }
