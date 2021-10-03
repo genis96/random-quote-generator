f5d0058c31c3b848713567a5c27ab4b39e874212
Genis Bardales <genis96@yahoo.com>
2021-10-03 01:14:29 -0400

    complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import './App.css';
 import fbIcon from './images/facebook.svg';
 import twitterIcon from './images/twitter.svg';
