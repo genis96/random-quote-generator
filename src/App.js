5a0abacff631712d5a8303189d8c1f97f3c5ce88
Genis Bardales <genis96@yahoo.com>
2021-10-02 23:17:50 -0400

    stoic api works inside the console

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Generate from './Generate';
+
 
 function App() {
+
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  useEffect(() => {
+    axios.get(
+      'https://stoicquotesapi.com/v1/api/quotes/random'
+    )
+    .then(res => {
+      setQuote(res.data);
+      console.log(res.data)
+    }).catch(error => console.log(error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi</h1>
     </div>
   );
 }
